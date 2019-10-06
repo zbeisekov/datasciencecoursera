44cce1138faefc3a76adc0a8bd1b5db5aabe40ef
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-10-06 11:03:01 +0200

    Passing test on the worst pneumonia hospital

--- PA3/tests/test_rankall.R
+++ PA3/tests/test_rankall.R
@@ -2,14 +2,28 @@ setwd("..")
 source("rankall.R")
 
 test_that("20th hospitals for heart attack",{
-    test_data <- read.csv("tests/heartattack20.csv",na.strings = "<NA>")
+    test_data <- read.csv2("tests/heartattack20.csv",na.strings = "<NA>")
     test <- data.frame( hospital = test_data$hospital,
                         state = test_data$state,
                         row.names = test_data$row.names)
     result <- rankall("heart attack", 20)
-    result <- result[1:10,]
-    print(result$hospital)
-    print(test$hospital)
+    result <- head(result, 10)
+    ##print(result$hospital)
+    ##print(test$hospital)
     expect_equal(as.character(result$hospital), as.character(test$hospital))
     expect_equal(as.character(result$state), as.character(test$state))
 })
+
+test_that("3 worst hospitals for pneumonia",{
+    test_data <- read.csv2("tests/pneumonia_worst.csv",na.strings = "<NA>")
+    test <- data.frame( hospital = test_data$hospital,
+                        state = test_data$state,
+                        row.names = test_data$row.names)
+    result <- rankall("pneumonia", "worst")
+    result <- tail(result, 3)
+    ##print(result$hospital)
+    ##print(test$hospital)
+    expect_equal(as.character(result$hospital), as.character(test$hospital))
+    expect_equal(as.character(result$state), as.character(test$state))
+})
+
