0f7d01672217dbdfc8628ed4fb54b1ee8b6fafa5
Zhenis Beisekov <zhenis.beisekov@gmail.com>
2019-10-06 11:12:14 +0200

    Passing all the tests for rankall

--- PA3/tests/test_rankall.R
+++ PA3/tests/test_rankall.R
@@ -8,8 +8,6 @@ test_that("20th hospitals for heart attack",{
                         row.names = test_data$row.names)
     result <- rankall("heart attack", 20)
     result <- head(result, 10)
-    ##print(result$hospital)
-    ##print(test$hospital)
     expect_equal(as.character(result$hospital), as.character(test$hospital))
     expect_equal(as.character(result$state), as.character(test$state))
 })
@@ -21,9 +19,17 @@ test_that("3 worst hospitals for pneumonia",{
                         row.names = test_data$row.names)
     result <- rankall("pneumonia", "worst")
     result <- tail(result, 3)
-    ##print(result$hospital)
-    ##print(test$hospital)
     expect_equal(as.character(result$hospital), as.character(test$hospital))
     expect_equal(as.character(result$state), as.character(test$state))
 })
 
+test_that("10 best hospitals for heart failure from the tail",{
+    test_data <- read.csv2("tests/heart_failure_best_tail.csv",na.strings = "<NA>")
+    test <- data.frame( hospital = test_data$hospital,
+                        state = test_data$state,
+                        row.names = test_data$row.names)
+    result <- rankall("heart failure")
+    result <- tail(result, 10)
+    expect_equal(as.character(result$hospital), as.character(test$hospital))
+    expect_equal(as.character(result$state), as.character(test$state))
+})
